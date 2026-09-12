1ace5ca10731137c55acf4c83dcc7fa0ee2ed70c
israeladamservice-cyber <israeladamservice@gmail.com>
2026-09-12 04:12:33 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,8 +2,8 @@
 import { useState, useEffect, useRef } from 'react';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://dcdvbglobtmeppfceapj.supabase.co';
-const supabaseAnonKey = 'sb_publishable_mzHTU0PoV5vmNeVUFG2eqw_-OWu0a0-1';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export default function Home() {
